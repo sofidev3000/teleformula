e1820de95ee48df5718bca5988cf5adb0a38cdcc
SofiDev <angiemeow89@gmail.com>
2024-10-09 15:06:04 -0600

    refactor

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://teleformula.com.mx',
-  integrations: [tailwind()]
+  integrations: []
 });
